3cdb9a9e4274197a8316119a84f85571480ab512
olabode <bodsul@gmail.com>
2018-07-14 22:23:42 -0700

    add some german traffic sign examples

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 wget -nc https://s3.amazonaws.com/video.udacity-data.com/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
 
-unzip traffic-signs-data
+unzip -n traffic-signs-data
